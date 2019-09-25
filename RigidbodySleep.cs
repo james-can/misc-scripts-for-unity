61ac217f40d5b2cfd9e70c890c12e8fdec354349
James Canada <jamesccanada@gmail.com>
2019-09-25 18:53:50 -0500

    add RigidbodySleep.cs

---
+++ RigidbodySleep.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodySleep : MonoBehaviour
+{
+    List<Rigidbody> children;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        Rigidbody[] childArray = GetComponentsInChildren<Rigidbody>();
+        children = new List<Rigidbody>();
+        children.AddRange(childArray);
+        
+        foreach(var child in children)
+        {
+            child.Sleep();
+        }
+
+        //rb = GetComponent<Rigidbody>();
+        //rb.Sleep();
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
